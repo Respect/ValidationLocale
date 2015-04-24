17a75509608cfdfdcd966f9ecb386edddc52926b
Henrique Moody <henriquemoody@gmail.com>
2015-04-24 19:42:47 -0300

    Create country subdivision rule for "OM"

---
+++ Rules/CountrySubdivision/OmCountrySubdivision.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Respect\Validation\Rules\CountrySubdivision;
+
+use Respect\Validation\Rules\AbstractSearcher;
+
+/**
+ * Validator for Oman country subdivision.
+ *
+ * ISO 3166-1 alpha-2: OM
+ *
+ * @link http://www.geonames.org/OM/administrative-division-oman.html
+ */
+class OmCountrySubdivision extends AbstractSearcher
+{
+    public $haystack = array(
+        'BA', // Al Batinah South
+        'BU', // Al Buraymī
+        'DA', // Ad Dakhiliyah
+        'MA', // Masqat
+        'MU', // Musandam
+        'SH', // Ash Sharqiyah South
+        'WU', // Al Wusta
+        'ZA', // Az Zahirah
+        'ZU', // Zufar
+    );
+
+    public $compareIdentical = true;
+}

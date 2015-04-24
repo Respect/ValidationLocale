3fa6fd0629cb3d28eaf0f5c51562e4767a1dae0f
Henrique Moody <henriquemoody@gmail.com>
2015-04-24 19:42:47 -0300

    Create country subdivision rule for "NR"

---
+++ Exceptions/CountrySubdivision/NrCountrySubdivisionException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Respect\Validation\Exceptions\CountrySubdivision;
+
+use Respect\Validation\Exceptions\CountrySubdivisionException;
+
+/**
+ * Exception class for Nauru country subdivision.
+ *
+ * ISO 3166-1 alpha-2: NR
+ */
+class NrCountrySubdivisionException extends CountrySubdivisionException
+{
+    public static $defaultTemplates = array(
+        self::MODE_DEFAULT => array(
+            self::STANDARD => '{{name}} must be a country subdivision of Nauru',
+        ),
+        self::MODE_NEGATIVE => array(
+            self::STANDARD => '{{name}} must not be a country subdivision of Nauru',
+        ),
+    );
+}

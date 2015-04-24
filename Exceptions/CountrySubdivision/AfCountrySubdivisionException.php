641ab8157740213db6d1cf9818581c397c7812d0
Henrique Moody <henriquemoody@gmail.com>
2015-04-24 19:42:16 -0300

    Create country subdivision rule for "AF"

---
+++ Exceptions/CountrySubdivision/AfCountrySubdivisionException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Respect\Validation\Exceptions\CountrySubdivision;
+
+use Respect\Validation\Exceptions\CountrySubdivisionException;
+
+/**
+ * Exception class for Afghanistan country subdivision.
+ *
+ * ISO 3166-1 alpha-2: AF
+ */
+class AfCountrySubdivisionException extends CountrySubdivisionException
+{
+    public static $defaultTemplates = array(
+        self::MODE_DEFAULT => array(
+            self::STANDARD => '{{name}} must be a country subdivision of Afghanistan',
+        ),
+        self::MODE_NEGATIVE => array(
+            self::STANDARD => '{{name}} must not be a country subdivision of Afghanistan',
+        ),
+    );
+}

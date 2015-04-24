d2f66d45c4a366aa63ea92b6bb1df486f03e86ec
Henrique Moody <henriquemoody@gmail.com>
2015-04-24 19:42:42 -0300

    Create country subdivision rule for "MH"

---
+++ Exceptions/CountrySubdivision/MhCountrySubdivisionException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Respect\Validation\Exceptions\CountrySubdivision;
+
+use Respect\Validation\Exceptions\CountrySubdivisionException;
+
+/**
+ * Exception class for Marshall Islands country subdivision.
+ *
+ * ISO 3166-1 alpha-2: MH
+ */
+class MhCountrySubdivisionException extends CountrySubdivisionException
+{
+    public static $defaultTemplates = array(
+        self::MODE_DEFAULT => array(
+            self::STANDARD => '{{name}} must be a country subdivision of Marshall Islands',
+        ),
+        self::MODE_NEGATIVE => array(
+            self::STANDARD => '{{name}} must not be a country subdivision of Marshall Islands',
+        ),
+    );
+}
